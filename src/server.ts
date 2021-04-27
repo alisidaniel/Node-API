624edd255bd6aeafb98bdb26a2bf22c6e34422c5
Oparaugo Melody Ifeanyi <melodyleonard7@gmail.com>
2021-04-27 17:08:00 +0100

    finished alias configuration

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,24 @@
-import http from "http";
-import express, {Application} from "express";
-import cors from "cors";
-import config from "@config/config";
-import corsOptions from "@utils/corsPermissions";
+// MODULE IMPORTS
+import 'module-alias/register';
+import http from 'http';
+import express, { Application } from 'express';
+import cors from 'cors';
+import config from '@config/config';
+import corsOptions from '@utils/corsPermissions';
 
 const app: Application = express();
 
-//  MIDDLEWARES
-app.use(express.urlencoded({extended:false}));
+//*  MIDDLEWARES */
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors(corsOptions));
 
-
+//* SERVER */
+const httpServer = http.createServer(app);
 try {
-    app.listen(config.server.port, () => {
-        console.info("Server running on port: ", `${config.server.port}`);
+    httpServer.listen(config.server.port, () => {
+        console.info(`Server running on port: ${config.server.port}`);
     });
-}catch(e){
-    console.log(e);
+} catch (err) {
+    console.log(err);
 }
